b6fc2efd2d0bd570c8969dc00223069bec949538
Sam <sampert+githubsux@hotmail.co.uk>
2025-03-03 07:34:04 +0000

    chore: move manual eprintln and exit calls to be a macro

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,19 @@ mod profile;
 mod meta;
 mod error;
 
+macro_rules! error_and_exit {
+    ($format: expr) => {
+        eprint!("ERROR: ");
+        eprintln!($format);
+        exit(-1);
+    };
+    ($format: expr, $($arg:tt)*) => {
+        eprint!("ERROR: ");
+        eprintln!($format, format_args!($($arg)*));
+        exit(-1);
+    };
+}
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct CmdlineArgs {
@@ -48,24 +61,22 @@ fn main() {
     // check at your own risk.
     if env::consts::OS != "linux" {
         println!("Dotulous is only supported on Linux.");
-        exit(-1);
+        exit(0);
     }
 
     let home_folder: String = match env::var("HOME") {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Unable to find suitable home folder: {e}"); exit(-1); }
+        Err(e) => { error_and_exit!("Unable to find suitable home folder: {e}"); }
     };
     let manifest_path: String = format!("{}/.dotulous/", home_folder);
     let manifest_location: &Path = Path::new(&manifest_path);
     if !manifest_location.exists() {
         if let Err(e) = fs::create_dir_all(manifest_location) {
-            eprintln!("ERROR: Unable to create dotulous folder: {e}");
-            exit(-1);
+            error_and_exit!("Unable to create dotulous folder: {e}");
         }
         let meta: Meta = Meta::new();
         if let Err(e) = meta.save_meta(manifest_location) {
-            eprintln!("ERROR: Failed to save meta: {e}");
-            exit(-1);
+            error_and_exit!("Failed to save meta: {e}");
         }
         println!("NOTE: Created dotulous folder at {manifest_path}");
         println!("NOTE: This is where your dotfile configurations will be!");
@@ -80,7 +91,7 @@ fn main() {
         Action::Status { } => {
             let meta: Meta = match Meta::load_meta(manifest_location) {
                 Ok(r) => r,
-                Err(e) => { eprintln!("ERROR: Could not load current meta: {e}"); exit(-1); },
+                Err(e) => { error_and_exit!("Could not load current meta: {e}"); },
             };
             let current_profile: Option<String> = meta.current_profile_name();
             if let Some(profile_name) = current_profile {
@@ -94,7 +105,7 @@ fn main() {
             // Scan for all available profiles 
             let paths = match fs::read_dir(manifest_location) {
                 Ok(r) => r,
-                Err(e) => { eprintln!("ERROR: Failed to read from directory \"{manifest_location:?}\": {e}"); exit(-1); }
+                Err(e) => { error_and_exit!("Failed to read from directory \"{manifest_location:?}\": {e}"); }
             };
             for path in paths {
                 let Ok(path) = path else {
@@ -122,19 +133,16 @@ fn action_create_profile(manifest_location: &Path, profile_name: &str) {
     let folder_path: &Path = Path::new(&folder_name);
     let full_path: PathBuf = manifest_location.join(folder_path);
     if full_path.exists() {
-        eprintln!("ERROR: Profile path \"{full_path:?}\" already exists!");
-        exit(-1);
+        error_and_exit!("Profile path \"{full_path:?}\" already exists!");
     }
     if let Err(e) = fs::create_dir_all(&full_path) {
-        eprintln!("ERROR: Unable to create folder \"{full_path:?}\": {e}");
-        exit(-1);
+        error_and_exit!("Unable to create folder \"{full_path:?}\": {e}");
     }
 
     // Create the manifest inside of it
     let manifest: DotfileProfile = DotfileProfile::new(profile_name, &full_path);
     if let Err(e) = manifest.save_manifest() {
-        eprintln!("ERROR: Failed to save profile manifest for \"{profile_name}\": {e}");
-        exit(-1);
+        error_and_exit!("Failed to save profile manifest for \"{profile_name}\": {e}");
     }
 
     println!("Created new profile at: {}", full_path.to_str().unwrap());
@@ -143,50 +151,48 @@ fn action_create_profile(manifest_location: &Path, profile_name: &str) {
 fn action_unload_profile(manifest_location: &Path) {
     let home_folder: String = match env::var("HOME") {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Unable to find suitable home folder: {e}"); exit(-1); }
+        Err(e) => { error_and_exit!("Unable to find suitable home folder: {e}"); }
     };
     let home_path: &Path = Path::new(&home_folder);
     println!("Using home folder: {:?}", home_path);
 
     let mut meta: Meta = match Meta::load_meta(manifest_location) {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Could not load current meta: {e}"); exit(-1); },
+        Err(e) => { error_and_exit!("Could not load current meta: {e}"); },
     };
     let Some(current_profile_name) = meta.current_profile_name() else {
-        eprintln!("No currently loaded profile was found. Nothing to do.");
-        exit(-1);
+        error_and_exit!("No currently loaded profile was found. Nothing to do.");
     };
 
     let profile: DotfileProfile = match DotfileProfile::find_profile(manifest_location, &current_profile_name) {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Could not find currently loaded profile {current_profile_name}: {e}"); exit(-1); },
+        Err(e) => { error_and_exit!("Could not find currently loaded profile {current_profile_name}: {e}"); },
     };
     profile.unload_profile_from_system(home_path);
 
     meta.empty_current_profile();
     if let Err(e) = meta.save_meta(manifest_location) {
-        eprintln!("ERROR: Failed to save meta: {e}");
-        exit(-1);
+        error_and_exit!("Failed to save meta: {e}");
     }
 }
 
 fn action_load_profile(manifest_location: &Path, profile_name: &str) {
     let home_folder: String = match env::var("HOME") {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Unable to find suitable home folder: {e}"); exit(-1); }
+        Err(e) => { error_and_exit!("Unable to find suitable home folder: {e}"); }
     };
     let home_path: &Path = Path::new(&home_folder);
     println!("Using home folder: {:?}", home_path);
 
     let mut meta: Meta = match Meta::load_meta(manifest_location) {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Could not load current meta: {e}"); exit(-1); },
+        Err(e) => { error_and_exit!("Could not load current meta: {e}"); },
     };
     if let Some(current_profile_name) = meta.current_profile_name() {
         // TODO: Handle this with redundant profile data in the meta
         let current_profile: DotfileProfile = match DotfileProfile::find_profile(manifest_location, &current_profile_name) {
             Ok(r) => r,
-            Err(e) => { eprintln!("ERROR: Could not find currently loaded profile {current_profile_name}: {e}"); exit(-1); },
+            Err(e) => { error_and_exit!("Could not find currently loaded profile {current_profile_name}: {e}"); },
         };
         current_profile.unload_profile_from_system(home_path);
         println!();
@@ -194,7 +200,7 @@ fn action_load_profile(manifest_location: &Path, profile_name: &str) {
 
     let profile: DotfileProfile = match DotfileProfile::find_profile(manifest_location, profile_name) {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Failed to load profile \"{profile_name}\": {e}"); exit(-1); },
+        Err(e) => { error_and_exit!("Failed to load profile \"{profile_name}\": {e}"); },
     };
 
     if !meta.is_trusted(&profile.repo_path) {
@@ -204,7 +210,9 @@ fn action_load_profile(manifest_location: &Path, profile_name: &str) {
         println!();
         println!("Do you trust this profile? (y/N)");
         let mut input: String = String::new();
-        io::stdin().read_line(&mut input).expect("Could not read input.");
+        if let Err(e) = io::stdin().read_line(&mut input) {
+            error_and_exit!("Failed to read from stdin: {e}");
+        }
         if input.trim().to_lowercase() != "y" {
             println!("Quitting...");
             exit(-1);
@@ -217,18 +225,16 @@ fn action_load_profile(manifest_location: &Path, profile_name: &str) {
 
     meta.set_current_profile(&profile);
     if let Err(e) = meta.save_meta(manifest_location) {
-        eprintln!("ERROR: Failed to save meta for \"{profile_name}\": {e}");
-        exit(-1);
+        error_and_exit!("Failed to save meta for \"{profile_name}\": {e}");
     }
 }
 
 fn action_fill_profile(manifest_location: &Path, profile_name: &str) {
     let mut profile: DotfileProfile = match DotfileProfile::find_profile(manifest_location, profile_name) {
         Ok(r) => r,
-        Err(e) => { eprintln!("ERROR: Failed to load profile \"{profile_name}\": {e}"); exit(-1); },
+        Err(e) => { error_and_exit!("Failed to load profile \"{profile_name}\": {e}"); },
     };
     if let Err(e) = profile.fill_files() {
-        eprintln!("ERROR: Failed to fill profile files for \"{profile_name}\": {e}");
-        exit(-1);
+        error_and_exit!("Failed to fill profile files for \"{profile_name}\": {e}");
     }
 }
